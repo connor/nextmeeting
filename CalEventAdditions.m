2dfd486176e7c5a791208b1f779c169735d4adeb
nathan spindel <nathans@gmail.com>
2012-10-30 13:56:09 -0700

    Mountain Lion compatibility

--- CalEventAdditions.m
+++ CalEventAdditions.m
@@ -1,19 +1,19 @@
 /* Public domain */
 
 #import "CalEventAdditions.h"
-#import "iCal.h"
+#import "Calendar.h"
 
 
 @implementation CalEvent (CalEventAdditions)
 
 - (void)show {
-	iCalApplication *iCal = [SBApplication applicationWithBundleIdentifier:@"com.apple.iCal"];
-	[iCal activate];
+	CalendarApplication *calendarApp = [SBApplication applicationWithBundleIdentifier:@"com.apple.iCal"];
+	[calendarApp activate];
 	
-	NSArray *matchingCalendars = [[iCal calendars] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"uid == %@", self.calendar.uid]];
+	NSArray *matchingCalendars = [[calendarApp calendars] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"uid == %@", self.calendar.uid]];
     
 	if (matchingCalendars.count > 0) {
-		iCalCalendar *containingCalendar = [matchingCalendars lastObject];
+		CalendarCalendar *containingCalendar = [matchingCalendars lastObject];
 		NSArray *matchingEvents = [[containingCalendar events] filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"uid == %@", self.uid]];
         
 		if (matchingEvents.count > 0) {
